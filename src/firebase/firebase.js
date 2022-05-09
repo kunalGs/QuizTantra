ac759e74829aa9cf9094f944a30592ae101b6bef
kunalGs <kunal.sinha@iiitb.ac.in>
2022-05-09 16:52:04 +0530

    env variables use in jenkins

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/storage'
 import 'firebase/analytics'
 import { auth } from "firebase";
 const firebaseConfig = {
-	apiKey: process.env.REACT_APP_API_KEY,
-	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-	projectId:  process.env.REACT_APP_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_SENDER_ID,
-	appId: process.env.REACT_APP_APP_ID
+	apiKey: ${REACT_APP_API_KEY},
+	authDomain: ${REACT_APP_AUTH_DOMAIN},
+	projectId:  ${REACT_APP_PROJECT_ID},
+	storageBucket: ${REACT_APP_STORAGE_BUCKET},
+	messagingSenderId: ${REACT_APP_SENDER_ID},
+	appId: ${REACT_APP_APP_ID}
 };
   
 // apiKey: "AIzaSyBWXFGj6AHAMGo_sO1cTy_E1oknPL7vKRY",
