df0a21bcc67526b4900a746334f0a43ae0ea9e99
kunalGs <kunal.sinha@iiitb.ac.in>
2022-05-12 00:12:36 +0530

    firebase Testing

--- src/fire.test.js
+++
@@ -1,5 +0,0 @@
-firebase.initializeTestApp({
-    projectId: "quiztantra",
-    auth: { uid: "qicBSAKDHpPdg6B5PZZ31VRjOyE3", email: "kunalgsinha@gmail.com" }
-  });
-
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const server = require('./src/server');
+const server = require('./backend/src/server');
 module.exports = server
 
 
